2efb64aa6b6b5859f6578b0a35098e602545ed59
Lucian NEAG <luxian.m@gmail.com>
2016-04-12 08:58:38 +0200

    HEKS-49 Disable auto-collapse when dragging

--- js/paragraphs_collapsible.widget.js
+++ js/paragraphs_collapsible.widget.js
@@ -29,40 +29,41 @@
                 });
             });
 
-            /*Check if user is dragging*/
-            $(paragraph_widget + ' .draggable .tabledrag-handle', context).mousedown(function(){
-                id = '#' + $(this).parents('table').attr('id');
-                draggable = paragraph_widget + ' ' + id + ' > tbody > tr.draggable';
-                $(draggable).addClass('collapsed');
-
-                /*Add id to current paragraph to scroll to it at the end*/
-                if ($(draggable + '.drag').attr('id')) {
-                    $(draggable + '.drag').removeAttr('id');
-                } else {
-                    $(draggable + '.drag').attr('id', 'edit-paragraph');
-                }
-
-                $('html, body').animate({
-                    scrollTop: $("#edit-paragraph").offset().top - 500
-                }, 0);
-            });
-
-            /*Check if user is finished dragging*/
-            if( $(paragraph_widget + ' .draggable').length ) {
-                $('body', context).mouseup(function(){
-                    if(!$(event.target).closest('.paragraph-toggle').length) {
-                        $(paragraph_widget).not('.collapsed-items').find('.draggable').removeClass('collapsed');
-                    }
-
-                    /*Scroll to*/
-                    if( $('#edit-paragraph', context).length ) {
-                        $('html, body').animate({
-                            scrollTop: $("#edit-paragraph").offset().top - 100
-                        }, 0);
-                        $(paragraph_widget + ' .draggable').removeAttr('id');
-                    }
-                });
-            }
+//          HEKS-49: Temporary disable autocollapse on drag and drop
+//             /*Check if user is dragging*/
+//             $(paragraph_widget + ' .draggable .tabledrag-handle', context).mousedown(function(){
+//                 id = '#' + $(this).parents('table').attr('id');
+//                 draggable = paragraph_widget + ' ' + id + ' > tbody > tr.draggable';
+//                 $(draggable).addClass('collapsed');
+//
+//                 /*Add id to current paragraph to scroll to it at the end*/
+//                 if ($(draggable + '.drag').attr('id')) {
+//                     $(draggable + '.drag').removeAttr('id');
+//                 } else {
+//                     $(draggable + '.drag').attr('id', 'edit-paragraph');
+//                 }
+//
+//                 $('html, body').animate({
+//                     scrollTop: $("#edit-paragraph").offset().top - 500
+//                 }, 0);
+//             });
+//
+//             /*Check if user is finished dragging*/
+//             if( $(paragraph_widget + ' .draggable').length ) {
+//                 $('body', context).mouseup(function(){
+//                     if(!$(event.target).closest('.paragraph-toggle').length) {
+//                         $(paragraph_widget).not('.collapsed-items').find('.draggable').removeClass('collapsed');
+//                     }
+//
+//                     /*Scroll to*/
+//                     if( $('#edit-paragraph', context).length ) {
+//                         $('html, body').animate({
+//                             scrollTop: $("#edit-paragraph").offset().top - 100
+//                         }, 0);
+//                         $(paragraph_widget + ' .draggable').removeAttr('id');
+//                     }
+//                 });
+//             }
         }
     };
 })(jQuery);
