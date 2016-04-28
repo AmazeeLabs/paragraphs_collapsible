052e4f3c036eb81831dd46bd10826fdbc74086d3
Victor Künzig <victor@amazeelabs.com>
2016-04-28 11:38:43 +0200

    HEKS-85 removing old code and replacing it with collapsible heks logic

--- js/paragraphs_collapsible.widget.js
+++ js/paragraphs_collapsible.widget.js
@@ -1,69 +1,72 @@
 (function ($) {
-    /**
-     * Paragraphs Drag&Drop functions
-     * */
-    Drupal.behaviors.paragraphs = {
-        attach: function (context, settings) {
-            paragraph_widget = '.field--widget-entity-reference-paragraphs'; /*Paragraph container*/
+  /**
+   * Paragraphs Drag&Drop functions
+   * */
+  Drupal.behaviors.paragraphs = {
+    attach: function (context, settings) {
+      var paragraphGuide = '> td > div > .form-wrapper > .paragraph-type-top';
 
-            /*Set toggles*/
-            $(paragraph_widget + ' .field-multiple-table .field-label', context).each(function(){
-                if (!$(this).find('.paragraph-toggle').length) {
-                    $(this).append('<a class="paragraph-toggle">Collapse</a>');
-                }
-            });
+      /**
+       * Setting up all the toggler and reference attributes
+       * */
+      $('table.field-multiple-table .field-label').each(function(paragraphIndex){
+        var $this = $(this);
 
-            /*Toggle function*/
-            $(paragraph_widget +' .paragraph-toggle', context).each(function(e){
-                $(this).click(function(e){
-                    e.preventDefault();
-                    id = '#' + $(this).parents('table').attr('id');
-                    draggable = id + ' > tbody > tr.draggable';
-                    $(paragraph_widget).toggleClass('collapsed-items');
-                    $(draggable).toggleClass('collapsed');
-                    if ( $(this).text() == 'Collapse' ) {
-                        $(this).text('Expand');
-                    } else {
-                        $(this).text('Collapse');
-                    }
-                });
-            });
+        // set reference attribute for the table
+        var $table = $this.parents('table.field-multiple-table').first();
+        $table.attr('data-paragraph-reference', paragraphIndex);
 
-//          HEKS-49: Temporary disable autocollapse on drag and drop
-//             /*Check if user is dragging*/
-//             $(paragraph_widget + ' .draggable .tabledrag-handle', context).mousedown(function(){
-//                 id = '#' + $(this).parents('table').attr('id');
-//                 draggable = paragraph_widget + ' ' + id + ' > tbody > tr.draggable';
-//                 $(draggable).addClass('collapsed');
-//
-//                 /*Add id to current paragraph to scroll to it at the end*/
-//                 if ($(draggable + '.drag').attr('id')) {
-//                     $(draggable + '.drag').removeAttr('id');
-//                 } else {
-//                     $(draggable + '.drag').attr('id', 'edit-paragraph');
-//                 }
-//
-//                 $('html, body').animate({
-//                     scrollTop: $("#edit-paragraph").offset().top - 500
-//                 }, 0);
-//             });
-//
-//             /*Check if user is finished dragging*/
-//             if( $(paragraph_widget + ' .draggable').length ) {
-//                 $('body', context).mouseup(function(){
-//                     if(!$(event.target).closest('.paragraph-toggle').length) {
-//                         $(paragraph_widget).not('.collapsed-items').find('.draggable').removeClass('collapsed');
-//                     }
-//
-//                     /*Scroll to*/
-//                     if( $('#edit-paragraph', context).length ) {
-//                         $('html, body').animate({
-//                             scrollTop: $("#edit-paragraph").offset().top - 100
-//                         }, 0);
-//                         $(paragraph_widget + ' .draggable').removeAttr('id');
-//                     }
-//                 });
-//             }
+        // create overarching toggler (only if there is more than one)
+        if ($table.find('> tbody > tr').length > 1) {
+          $this.once('paragraph-toggle-once').append('<a class="paragraph-toggle" data-paragraph-reference="' + paragraphIndex + '">' + Drupal.t('Expand all') + '</a>');
         }
-    };
+
+        $table.find('> tbody > tr').once('paragraph-item-once').each(function(paragraphRowIndex){
+          var $row = $(this);
+
+          // set references for each row
+          $row.attr('data-row-reference', paragraphIndex + '-' + paragraphRowIndex);
+
+          // create toggler for each paragraph element
+          if ($row.find(paragraphGuide + ' + .paragraphs-subform').length) {
+            $row.find(paragraphGuide + ' > .paragraph-type-title').once('paragraph-item-toggle-once').append('<a class="paragraph-item-toggle" data-row-reference="' + paragraphIndex + '-' + paragraphRowIndex + '">[+]</a>');
+          }
+        });
+      });
+
+      /**
+       * Complete paragraph toggler
+       * */
+      $('.paragraph-toggle', context).once('paragraph-rows-toggle').on('click', function(e){
+        e.preventDefault();
+
+        var $toggle = $(this),
+            $rows = $('tr[data-row-reference*="' + $toggle.attr('data-paragraph-reference') + '-"]');
+
+        if ($toggle.text() == 'Collapse all') {
+          $toggle.text(Drupal.t('Expand all'));
+          $rows.removeClass('expanded').find(paragraphGuide + ' > .paragraph-type-title .paragraph-item-toggle').text('[+]');
+        } else {
+          $toggle.text(Drupal.t('Collapse all'));
+          $rows.addClass('expanded').find(paragraphGuide + ' > .paragraph-type-title .paragraph-item-toggle').text('[-]');
+        }
+      });
+
+      /**
+       * Individual paragraph element toggler
+       * */
+      $('.paragraph-item-toggle', context).once('paragraph-row-toggle').on('click', function(e){
+        e.preventDefault();
+
+        var $toggle = $(this);
+
+        // expand / collapse row
+        $('tr[data-row-reference="' + $toggle.attr('data-row-reference') + '"]').toggleClass('expanded');
+
+        // visually show expanded / collapsed
+        $toggle.text() == '[+]' ? $toggle.text('[-]') : $toggle.text('[+]');
+      });
+    }
+  };
+
 })(jQuery);
